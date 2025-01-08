9aed6fd7befe4ed1b9e2c7b796a1522df47d18a8
Linabenhaj <Benhaj.L@outlook.com>
2025-01-08 09:42:09 +0100

    basis voor Personeelslid

---
+++ src/Personeelslid.java
@@ -0,0 +1,12 @@
+public class Personeelslid {
+    String naam;
+    int leeftijd;
+    String adres;
+
+
+    public Personeelslid(String naam, int leeftijd, String adres) {
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+        this.adres = adres;
+    }
+}
