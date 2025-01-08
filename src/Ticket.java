49bd7131dd892c5c950a1563c3bed6c4a13a925f
Linabenhaj <Benhaj.L@outlook.com>
2025-01-08 11:05:01 +0100

    verwijderen van aanroeping maakTicketAan

--- src/Ticket.java
+++ src/Ticket.java
@@ -10,9 +10,3 @@ public class Ticket{
         this.vlucht = vlucht;
     }
 
-public void maakTicketAan(Passagier passagier, String klasse, Vlucht vlucht) {
-    Ticket ticket = new ticket(passagier, klasse, vlucht);
-    passagier.geefTicket(ticket);
-    System.out.println("Ticket aangemaakt voor passagier " + passagier.naam);
-}
-
