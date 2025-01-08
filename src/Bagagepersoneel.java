ee26aa3e4189055f107bcb1ed9c71318f542c110
Linabenhaj <Benhaj.L@outlook.com>
2025-01-08 09:39:07 +0100

    aanpassing restrictie bagagegewicht

--- src/Bagagepersoneel.java
+++ src/Bagagepersoneel.java
@@ -1,15 +1,14 @@
 public class BagagePersoneel extends Personeelslid {
-    public BagagePersoneel(String naam, int leeftijd, String adres){
+    public BagagePersoneel(String naam, int leeftijd, String adres) {
         super(naam, leeftijd, adres);
     }
 
-
     public boolean controleerBagage(Passagier passagier) {
-        if (passagier.getAantalBagagestukken() > 10){
-            System.out.println("Bagage van " + passagier.naam + " is te veel.");
+        if (passagier.getBagageGewicht() > 30) {
+            System.out.println("Bagage van" + passagier.naam + " is te zwaar.");
             return false;
+        }
         System.out.println("Bagage van " + passagier.naam + " is in orde.");
         return true;
     }
-
 }
