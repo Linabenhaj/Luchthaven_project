457f2e8c4e7c6da2d21eb0713c2043cb5168ba67
Linabenhaj <Benhaj.L@outlook.com>
2025-01-08 11:27:55 +0100

    flightcheck toegevoegd aan klasse piloot

--- src/Piloot.java
+++ src/Piloot.java
@@ -5,4 +5,15 @@ public class Piloot extends Personeelslid {
         super(naam, leeftijd, adres);
     }
 
+//flightcheck: interpretatie "als alle passagiers aan boord zijn"
 
+    public boolean flightCheck(Vlucht vlucht) {
+        if (vlucht.isVol()) {
+            System.out.println("De vlucht is vol dus de vliegtuig mag opstijgen. ");
+            return true;
+        } else {
+            System.out.println("De vlucht is nog niet vol dus de vliegtuig kan niet opstijgen.");
+            return false;
+        }
+    }
+}
