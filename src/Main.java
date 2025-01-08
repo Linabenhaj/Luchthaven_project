cb0f2764884f6812863e261326e52385ba9fc867
Linabenhaj <Benhaj.L@outlook.com>
2025-01-08 11:57:14 +0100

    switch case toegevoegd om keuzemenu voor gebruiker op te bouwen - niet af

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,53 @@ public class Main {
             int keuze = Integer.parseInt(scanner.nextLine());
             switch (keuze) {
                 case 1:
-                    System.out.println("Vluchtcode: ");
+                    System.out.print("Vluchtcode: ");
+                    String vluchtcode = scanner.nextLine();
+                    System.out.print("Bestemming:");
+                    String bestemming = scanner.nextLine();
+                    System.out.print("Aantal economy plaatsen: ");
+                    int economy = Integer.parseInt(scanner.nextLine());
+                    System.out.print("Aantal business plaatsen:");
+                    int business = Integer.parseInt(scanner.nextLine());
+
+                    Vlucht vlucht = new Vlucht(vluchtcode, bestemming, economy, business);
+                    vlucht = scanner.nextLina();
+
+                    break;
+                    case 2:
+                        System.out.print("Naam personeelslid: ");
+                        String naam = scanner.nextLine();
+                        System.out.print("Leeftijd: ");
+                        int leeftijd = Integer.parseInt(scanner.nextLine());
+                        System.out.print("Adres: ");
+                        String adres = scanner.nextLine();
+
+                        Personeelslid personeelslid = new Personeelslid(naam, leeftijd, adres);
+                        vluchtcode = scanner.nextLine();
+
+                        break;
+
+                        case 3:
+                            System.out.print("Naam passagier: ");
+                            String naam = scanner.nextLine();
+                            System.out.print("Leeftijd: ");
+                            int leeftijd = Integer.parseInt(scanner.nextLine());
+                            System.out.print("Adres: ");
+                            String adres = scanner.nextLine();
+// bagage toevoegen
+                            break;
+                            case 4:
+                                System.out.print("Kies een vluchtcode om informatie te printen:");
+   // hoe linken van de vluchtcode?
+
+                                break;
+                                case 5: luchthaven.exporteerAlleVluchtInfo();
+                                break;
+                                case 6:
+                                    System.out.println("Programma stoppen");
+
             }
         }
-    }   }
+        scanner.close();
+        }
+    }
