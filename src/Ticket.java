d6d47ff0a6bfb7ded7558c723f4ab6c888b372c6
Linabenhaj <Benhaj.L@outlook.com>
2025-01-09 15:16:12 +0100

    finale versie ticket

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,12 +1,14 @@
-public class Ticket{
+public class Ticket {
     Passagier passagier;
     String klasse;
     Vlucht vlucht;
 
 
-    public Ticket(Passagier passagier, String klasse, Vlucht vlucht){
+    public Ticket(Passagier passagier, String klasse, Vlucht vlucht) {
         this.passagier = passagier;
         this.klasse = klasse;
         this.vlucht = vlucht;
     }
+
+}
 
