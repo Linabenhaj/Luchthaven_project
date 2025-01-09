2ad6b146843b7d8bec38a9763429f79a81d2b6f5
Linabenhaj <Benhaj.L@outlook.com>
2025-01-09 21:38:20 +0100

    flightcheck verandert naar "is er een piloot en ten minste 1 passagier" dan mag die vliegen

--- src/Piloot.java
+++ src/Piloot.java
@@ -8,12 +8,25 @@ public class Piloot extends Personeelslid {
 //flightcheck: interpretatie "als alle passagiers aan boord zijn"
 
     public boolean flightCheck(Vlucht vlucht) {
-        if (vlucht.isVol()) {
-            System.out.println("De vlucht is vol dus de vliegtuig mag opstijgen. ");
-            return true;
-        } else {
-            System.out.println("De vlucht is nog niet vol dus de vliegtuig kan niet opstijgen.");
+        boolean isErEenPiloot = false;
+        for (Personeelslid personeelslid : vlucht.personeel) {
+            if (personeelslid instanceof Piloot) {
+                isErEenPiloot = true;
+                break;
+            }
+    }
+        if (!isErEenPiloot) {
+            System.out.println("Flightcheck niet oke. Er is geen piloot aan boord.");
             return false;
         }
+
+        if (vlucht.passagiers.isEmpty()) {
+            System.out.println("Flightcheck niet oke. Er zijn geen passagiers aan boord.");
+            return false;
+        }
+
+        System.out.println("Flightcheck oke! Alles is in orde, de vlucht kan opstijgen.");
+        return true;
     }
 }
+
